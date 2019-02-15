c053253599ff91875f17a519b96608f35d08c2f0
Ian Peter Tecuico <ipstecuico04@gmail.com>
2019-02-15 08:57:57 +0800

    Added service card directive

---
+++ app/modules/home/serviceCardDirective.js
@@ -0,0 +1,19 @@
+﻿/* global angular*/
+(function () {
+    var app = angular.module("app");
+
+    app.directive("serviceCard", function () {
+        return {
+            restrict: "E",
+            transclude: true,
+            templateUrl: "/app/modules/home/serviceCard.html",
+            scope: {
+                title: "@",
+                description: "@",
+                url: "@"
+            },
+            controller($scope) {
+            }
+        };
+    });
+}());
--- scripts/app/app.js
+++ scripts/app/app.js
@@ -141,6 +141,25 @@
 (function () {
     var app = angular.module("app");
 
+    app.directive("serviceCard", function () {
+        return {
+            restrict: "E",
+            transclude: true,
+            templateUrl: "/app/modules/home/serviceCard.html",
+            scope: {
+                title: "@",
+                description: "@",
+                url: "@"
+            },
+            controller($scope) {
+            }
+        };
+    });
+}());
+/* global angular*/
+(function () {
+    var app = angular.module("app");
+
     app.directive("projectCard", function () {
         return {
             restrict: "E",
